1d4f27c59642f81f966c54d156a1121557abaa22
Oskar Jansson <oskja067@student.liu.se>
2018-02-11 09:44:02 +0100

    updates: person.py to handle everything

--- scripts/person.py
+++ scripts/person.py
@@ -1,37 +1,25 @@
 import json
 import sys
+import argparse
+import urllib.request
 
-def parse(arg):
-    res = []
-    if arg[0] == '-':
-        res = parse_flag(arg[1:])
-    return res
 
-def parse_flag(flag):
-    keys = []
-    for c in flag:
-        if c == 'm':
-            keys.append('email_strict')
-        elif c == 't':
-            keys.append('phone_strict')
-        elif c == 'i':
-            keys.append('liu_id')
-        elif c == 'n':
-            keys.append('full_name_strict')
-        else:
-            raise Exception("Unknown flag {}".format(c))
-    return keys
 
 def main():
-    keys=['full_name_strict', 'email_strict', 'liu_id', 'phone_strict']
-    if len(sys.argv) > 1:
-        keys = []
-        for arg in sys.argv:
-           keys += parse(arg)
+    parser = argparse.ArgumentParser(description='Search for persons at LiU more easily')
+    parser.add_argument('-m','--mail', help='print mail to stdout')
+    parser.add_argument('-t','--tele', help='print telephone number to stdout')
+    parser.add_argument('-n','--name', help='print name to stdout')
+    parser.add_argument('names', nargs='+', help='the names to search LiU for')
+    args = parser.parse_args()
+    print(args.names)
+    url="https://search.liu.se/jellyfish/rest/apps/LiU/searchers/sitecore2_people_only?query=marco&lang=sv"
+    f = None
+    with urllib.request.urlopen(url) as request:
+        body = request.read()
+        f = json.loads(body.decode('utf-8'))
 
-    f = json.load(sys.stdin)
-    for person in f['documentList']['documents']:
-        for key in keys:
+        for person in f['documentList']['documents']:
             try:
                 tmp = person[key]
                 print( tmp[0] if key == 'phone_strict' else tmp)
